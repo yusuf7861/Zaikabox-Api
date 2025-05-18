efb2e4052de75006562e759ff79657a1c04c8291
Yusuf Jamal <72370431+yusuf7861@users.noreply.github.com>
2025-05-18 19:45:03 +0530

    initial commit

---
+++ src/main/java/tech/realworks/yusuf/zaikabox/ZaikaboxApiApplication.java
@@ -0,0 +1,13 @@
+package tech.realworks.yusuf.zaikabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaikaboxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaikaboxApiApplication.class, args);
+    }
+
+}
